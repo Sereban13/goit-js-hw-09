fa9284f71955f06b7a88bd18ad9725da06623cd0
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-04-20 09:47:15 +0200

    first_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+let colorId;
+
+startBtn.addEventListener('click', colorizer);
+stopBtn.addEventListener('click', colorizerStop);
+
+function colorizer () {
+    colorId = setInterval(getRandomHexColor, 1000);
+    startBtn.disabled = true;
+}
+
+function colorizerStop () {
+    clearInterval(colorId);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    return body.style.backgroundColor;
+}
